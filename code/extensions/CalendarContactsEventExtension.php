b2e7472dec3fbcf4bb0037704766f393fe9e8375
Todd Hossack <todd@tiraki.com>
2019-10-07 15:53:24 +1300

    Subsite support for default records. Sortable event contacts

--- code/extensions/CalendarContactsEventExtension.php
+++ code/extensions/CalendarContactsEventExtension.php
@@ -4,29 +4,52 @@
 class CalendarContactsEventExtension extends DataExtension {
 
     private static $many_many = array(
-		'Contacts' => 'CMDirectoryEntry'
+		'Contacts' => 'CMDirectoryPersonEntry'
 	);
+    
+    private static $many_many_extraFields = [
+        'Contacts' => [
+          'Sort' => 'Int'
+        ]
+    ];
     
     public function updateCMSFields(FieldList $fields)
     {
+        $contacts = $fields->fieldByName('Contacts');
+
         // Add tab
 		$fields->findOrMakeTab(
 			'Root.Contacts', _t('CalendarContactsEventExtension.ContactsTab','Contacts')
 		);
-
-        $options = CMDirectoryEntry::get()->map('ID','FullName')->toArray();
-        asort($options);
         
-        $contactsFields = ListboxField::create('Contacts',
-            _t('CalendarContactsEventExtension.Contacts','Contacts'),
-            $options, '', 5, true
+        // GridField
+        $gridFieldConf = GridFieldConfig_RecordEditor::create();
+        // Auto completer
+        $autoCompleter = new GridFieldAddExistingAutocompleter();
+        $autoCompleter->setResultsFormat('$FullName ($Email)')->setSearchFields(['LastName','Name','Email']);
+        $autoCompleter->setSearchList(CMDirectoryPersonEntry::get());
+        $gridFieldConf->addComponent($autoCompleter);
+        // Sorting - @todo Sorting relation on front end doesn't seem to work with SS3
+        if (class_exists('GridFieldOrderableRows')) {
+            $gridFieldConf->addComponent(new GridFieldOrderableRows('Sort'));
+        } elseif (class_exists('GridFieldSortableRows')) {
+            $gridFieldConf->addComponent(new GridFieldSortableRows('Sort'));
+        }
+        // Make field
+        $contactsField = GridField::create('Contacts', 
+            _t('CalendarContactsEventExtension.Contacts','Contacts'), 
+            $this->owner->SortedContacts(), 
+            $gridFieldConf
         );
-        
-        $fields->addFieldToTab('Root.Contacts',$contactsFields);
+
+        $fields->addFieldToTab('Root.Contacts',$contactsField);
     }
     
-    
-    
+    public function SortedContacts()
+    {
+        return $this->owner->Contacts()->sort('Sort','ASC');
+        
+    }
     public function onBeforeDelete() 
     {
         //$this->owner->Contacts()->removeAll();
--- code/extensions/CalendarDirectoryExtension.php
+++ code/extensions/CalendarDirectoryExtension.php
@@ -2,8 +2,11 @@
 
 class CalendarDirectoryExtension extends DataExtension {
 
-  private static $default_events_directory = "Event Organizers";
+  private static $default_events_directory = "Event Contacts";
   
+  private static $db = [
+      'EventContactsDirectory' => 'Boolean'
+  ];
   /**
    * Adds default groups
    */
@@ -12,20 +15,54 @@ class CalendarDirectoryExtension extends DataExtension {
         
         $className = $this->owner->class;
 
-        // Check for existing record
-        $res = DataModel::inst()->$className
-              ->filter(array('Name' => $defaultRecordName))
-              ->first();
-        if($res) {
-            return;
-        }
+        // Subsite support
+        $classInstance = Injector::inst()->create($className);
+        if(class_exists('Subsite') && $classInstance->hasDatabaseField('SubsiteID')) {
+            $subsites = Subsite::get();
+            $disableFilter = Subsite::$disable_subsite_filter;
+            Subsite::$disable_subsite_filter = true;
+            // Create default record for each subsite
+            foreach($subsites as $subsite) {
+                $res = DataModel::inst()->$className
+                  ->filter([
+                      'SubsiteID' => $subsite->ID,
+                      'EventContactsDirectory' => 1
+                  ])
+                  ->first();
+                
+                if(!$res) {
+                    $obj = DataModel::inst()->$className->newObject([
+                        'Name' => $defaultRecordName,
+                        'SubsiteID' => $subsite->ID,
+                        'EventContactsDirectory' => 1
+                    ]);
+                    $obj->write();
+                }
+            }
+            Subsite::$disable_subsite_filter = $disableFilter;
+        } else {
+            // Check for existing record
+            $res = DataModel::inst()->$className
+                  ->filter(['EventContactsDirectory' => 1])
+                  ->first();
+            if($res) {
+                return;
+            }
 
-        $obj = DataModel::inst()->$className->newObject([
-            'Name' => $defaultRecordName
-        ]);
-        $obj->write();
-   
-        DB::alteration_message("Added default record to $className table","created");
+            // Create one default record
+            $obj = DataModel::inst()->$className->newObject([
+                'Name' => $defaultRecordName,
+                'EventContactsDirectory' => 1
+            ]);
+            $obj->write();
+        }
         
+        DB::alteration_message("Added default record(s) to $className table","created");
 	}
+    
+    public function updateCMSFields(FieldList $fields)
+    {
+        // Remove events gridfield
+        $fields->removeByName('EventContactsDirectory');
+    }
 }
