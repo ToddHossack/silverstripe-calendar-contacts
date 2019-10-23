c1f021f6dd8a3e1e59dd1d9810f8b3785d74598d
Todd Hossack <todd@tiraki.com>
2019-10-23 21:06:17 +1300

    CConfig change. Restricted contacts gridfield from unsaved entities to avoid relation error

--- code/extensions/CalendarContactsEventExtension.php
+++ code/extensions/CalendarContactsEventExtension.php
@@ -9,40 +9,44 @@ class CalendarContactsEventExtension extends DataExtension {
     
     private static $many_many_extraFields = [
         'Contacts' => [
-          'Sort' => 'Int'
+            'Sort' => 'Int'
         ]
     ];
     
     public function updateCMSFields(FieldList $fields)
     {
-        $contacts = $fields->fieldByName('Contacts');
-
         // Add tab
 		$fields->findOrMakeTab(
 			'Root.Contacts', _t('CalendarContactsEventExtension.ContactsTab','Contacts')
 		);
-        
-        // GridField
-        $gridFieldConf = GridFieldConfig_RecordEditor::create();
-        // Auto completer
-        $autoCompleter = new GridFieldAddExistingAutocompleter();
-        $autoCompleter->setResultsFormat('$FullName ($Email)')->setSearchFields(['LastName','Name','Email']);
-        $autoCompleter->setSearchList(CMDirectoryPersonEntry::get());
-        $gridFieldConf->addComponent($autoCompleter);
-        // Sorting - @todo Sorting relation on front end doesn't seem to work with SS3
-        if (class_exists('GridFieldOrderableRows')) {
-            $gridFieldConf->addComponent(new GridFieldOrderableRows('Sort'));
-        } elseif (class_exists('GridFieldSortableRows')) {
-            $gridFieldConf->addComponent(new GridFieldSortableRows('Sort'));
+        if($this->owner->exists()) {
+            // GridField
+            $gridFieldConf = GridFieldConfig_RecordEditor::create();
+            // Auto completer
+            $autoCompleter = new GridFieldAddExistingAutocompleter();
+            $autoCompleter->setResultsFormat('$FullName ($Email)')->setSearchFields(['LastName','Name','Email']);
+            $autoCompleter->setSearchList(CMDirectoryPersonEntry::get());
+            $gridFieldConf->addComponent($autoCompleter);
+            // Sorting
+            if (class_exists('GridFieldOrderableRows')) {
+                $gridFieldConf->addComponent(new GridFieldOrderableRows('Sort'));
+            } elseif (class_exists('GridFieldSortableRows')) {
+                $gridFieldConf->addComponent(new GridFieldSortableRows('Sort'));
+            }
+            // Make field
+            $contactsField = GridField::create('Contacts', 
+                _t('CalendarContactsEventExtension.Contacts','Contacts'), 
+                $this->owner->SortedContacts(), 
+                $gridFieldConf
+            );
+            
+            $fields->addFieldToTab('Root.Contacts',$contactsField);
+            
+		} else {
+            $fields->addFieldToTab('Root.Contacts', LiteralField::create('NotSaved', "<p class='message warning'>"._t('CalendarContactsEventExtension.AddContactsAfterSaving', 'Contacts may be added after the event has been saved for the first time.').'</p>'));
         }
-        // Make field
-        $contactsField = GridField::create('Contacts', 
-            _t('CalendarContactsEventExtension.Contacts','Contacts'), 
-            $this->owner->SortedContacts(), 
-            $gridFieldConf
-        );
-
-        $fields->addFieldToTab('Root.Contacts',$contactsField);
+        
+        
     }
     
     public function SortedContacts()
@@ -55,5 +59,6 @@ class CalendarContactsEventExtension extends DataExtension {
         //$this->owner->Contacts()->removeAll();
 	}
 
+   
 
 }
